cf9a9778184ada120404b340d5c5f4909842c28f
BMC-Gordon0921 <2710464988@qq.com>
2025-01-30 01:22:49 +0800

    feat: confetti button

--- src/components/queryBalanceButton.tsx
+++ src/components/queryBalanceButton.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import { isOk, request } from "../utils/network";
 import { GetInfoResp } from "../data/interface/network";
 import { DollarCircleFilled } from "@ant-design/icons";
+import confetti from "canvas-confetti";
+import { HeartTwoTone } from "@ant-design/icons";
 
 export const QueryBalanceButton: React.FC = () => {
   const [info, setInfo] = useState<GetInfoResp | undefined>();
@@ -21,6 +23,14 @@ export const QueryBalanceButton: React.FC = () => {
   const queryBalance = () => {
     message.info("当前灵力值：" + String(info?.token_balance));
   };
+  const onConfetti = () => {
+    confetti({
+      particleCount: 200,
+      spread: 70,
+      origin: { x: 0.5, y: 0.5 },
+    });
+    message.success("守塔姬给你放个烟花！要加油哦~");
+  };
   return (
     <div>
       <FloatButton
@@ -28,6 +38,14 @@ export const QueryBalanceButton: React.FC = () => {
         onClick={queryBalance}
         type="primary"
         icon={<DollarCircleFilled />}
+        style={{ insetInlineEnd: 75 }}
+      />
+      <FloatButton
+        onClick={onConfetti}
+        type="primary"
+        icon={<HeartTwoTone twoToneColor="#eb2f96" />}
+        style={{ insetInlineEnd: 20 }}
+        tooltip="放个烟花吗？"
       />
     </div>
   );
